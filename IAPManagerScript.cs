0ebec5671d688ec0787e7a4bbbd63a3d285b8122
MrAladross <52119895+MrAladross@users.noreply.github.com>
2020-10-19 03:03:58 -0700

    Add files via upload
    sample IAP code. the referenced savescript just stores a bool for whether or not noads was purchased. There is only 1 IAP, noadvertisements, in this code sample.

---
+++ IAPManagerScript.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UI;
+
+
+// Deriving the Purchaser class from IStoreListener enables it to receive messages from Unity Purchasing.
+public class IAPManagerScript : MonoBehaviour, IStoreListener
+{
+    private static IStoreController m_StoreController;          // The Unity Purchasing system.
+    private static IExtensionProvider m_StoreExtensionProvider; // The store-specific Purchasing subsystems.
+    public static IAPManagerScript iapman { set; get; }//singleton, please do this differently, it's terrible
+    public AlreadyPurchasedScript noAdsAps;
+
+    public static string noAds = "noadvertisements";
+    //add more products here
+    
+
+    // Apple App Store-specific product identifier for the subscription product.
+    // private static string kProductNameAppleSubscription = "com.unity3d.subscription.new";
+    public void Awake()
+    {
+        iapman = this;
+    }
+
+    void Start()
+    {
+        // If we haven't set up the Unity Purchasing reference
+        if (m_StoreController == null)
+        {
+            // Begin to configure our connection to Purchasing
+            InitializePurchasing();
+        }
+    }
+
+    public void InitializePurchasing()
+    {
+        if (IsInitialized())
+        {
+            return;
+        }
+        
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        builder.AddProduct(noAds, ProductType.NonConsumable);
+        
+        
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+
+    private bool IsInitialized()
+    {
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+    
+    public void BuyNoAdvertisements()
+    {
+        if (SaveScript.ss.noAdvertisements == false)
+        {
+            BuyProductID(noAds);
+            Debug.Log("no ads");
+        }
+        else if (SaveScript.ss.noAdvertisements)
+        {
+            Debug.Log("ads don't exist bro");
+            noAdsAps.SetToPurchasedState();
+        }
+    }
+
+    
+
+    private void BuyProductID(string productId)
+    {
+        if (IsInitialized())
+        {
+            // ... look up the Product reference with the general product identifier and the Purchasing 
+            // system's products collection.
+            Product product = m_StoreController.products.WithID(productId);
+
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+               
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            {
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        else
+        {
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+
+    public void RestorePurchases()
+    {
+        // If Purchasing has not yet been set up ...
+        if (!IsInitialized())
+        {
+            // ... report the situation and stop restoring. Consider either waiting longer, or retrying initialization.
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        // If we are running on an Apple device ... 
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            // ... begin restoring purchases
+            Debug.Log("RestorePurchases started ...");
+
+            // Fetch the Apple store-specific subsystem.
+            var apple = m_StoreExtensionProvider.GetExtension<IAppleExtensions>();
+            // Begin the asynchronous process of restoring purchases. Expect a confirmation response in 
+            // the Action<bool> below, and ProcessPurchase if there are previously purchased products to restore.
+            apple.RestoreTransactions((result) => {
+                // The first phase of restoration. If no more responses are received on ProcessPurchase then 
+                // no purchases are available to be restored.
+                Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+            });
+        }
+        // Otherwise ...
+        else
+        {
+            // We are not running on an Apple device. No work is necessary to restore purchases.
+            Debug.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+        }
+    }
+    
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        Debug.Log("OnInitialized: PASS");
+
+        m_StoreController = controller;
+        m_StoreExtensionProvider = extensions;
+
+        //check here for any non-consumables that the person already owns
+        Product product = m_StoreController.products.WithID(noAds);
+        if (product!=null && product.hasReceipt)
+        {
+            SaveScript.ss.noAdvertisements = true;
+            SaveScript.ss.Save();
+            if (product.metadata.localizedTitle==noAds)
+            {
+                noAdsAps.SetToPurchasedState();
+            }
+        }
+    }
+
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+    
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, noAds, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            Debug.Log("ads were successfully removed");
+            SaveScript.ss.noAdvertisements = true;
+            SaveScript.ss.Save();
+            // TODO: The non-consumable item has been successfully purchased, grant this item to the player.
+        }
+       /* else if (String.Equals(args.purchasedProduct.definition.id, kProductIDSubscription, StringComparison.Ordinal))
+        {
+            Debug.Log(string.Format("ProcessPurchase: PASS. Product: '{0}'", args.purchasedProduct.definition.id));
+            // TODO: The subscription item has been successfully purchased, grant this to the player.
+        }*/
+        else
+        {
+            Debug.Log(string.Format("ProcessPurchase: FAIL. Unrecognized product: '{0}'", args.purchasedProduct.definition.id));
+        }
+        
+        return PurchaseProcessingResult.Complete;
+    }
+
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        
+        // PlayerObjectScript.pos.winText.gameObject.SetActive(true);
+
+        // PlayerObjectScript.pos.winText.text = "purchase failed";
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+}
